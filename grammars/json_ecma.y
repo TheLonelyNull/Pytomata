5dd5ab15635322dd97d5afecb547c03537450293
Christoff Rossouw <chris.rossouw5@gmail.com>
2021-01-23 14:50:38 +0200

    Gtestr converter mostly finished

---
+++ grammars/json_ecma.y
@@ -0,0 +1,48 @@
+%%
+
+initsymbol: value
+          ;
+
+value: object |
+       array |
+       number |
+       string |
+       'true' |
+       'false' |
+       'null'
+     ;
+
+
+membersoptional: |
+                 members
+               ;
+
+object: '{' membersoptional '}'
+      ;
+
+pairstar: |
+          ',' pair pairstar
+        ;
+
+members: pair pairstar
+       ;
+
+pair: string ':' value
+    ;
+
+elementsoptional: |
+                  elements
+                ;
+
+array: '[' elementsoptional ']'
+     ;
+
+valuestar: |
+           ',' value valuestar
+         ;
+
+elements: value valuestar
+        ;
+
+
+%%
--- gtestr_converter.py
+++ gtestr_converter.py
@@ -33,7 +33,7 @@ def parse_nonterms(file):
             started = True
 
         if started:
-            arr = re.split("\(|\)", line)
+            arr = re.split("nonterm\(|\)\.\n", line)
             term = arr[1]
             nonterms.append(term)
     return nonterms
@@ -53,19 +53,54 @@ def parse_rules(file, terms, nonterms):
 
         if started:
             arr = re.split("\((?=\d*:\d*)|\)\.\n|\)\.\Z", line)
-            contents = re.split("(?=.*\[),", arr[1])
-            rule_nr = int(re.split(":", contents[0])[0])
+            contents = re.split("(?=.*\[), ", arr[1])
             nonterm = contents[1].strip()
-            if rule_nr != i:
-                i = rule_nr
-                rules[nonterm] = list()
+            assert nonterm in nonterms
+            if nonterm not in rules:
+                rules[nonterm] = []
             rhs = contents[2].strip()
             rhs = rhs[1:-1]
-            rhs = re.split(', ', rhs)
-            for j in range(len(rhs)):
-                rhs[j] = rhs[j].strip()
-            rules[nonterm].append(rhs)
+            rhs_arr = re.split(', ', rhs)
+            for j in range(len(rhs_arr)):
+                rhs_arr[j] = rhs_arr[j].strip()
+            if len(rhs_arr) == 1 and rhs_arr[0] == "":
+                rules[nonterm].insert(0, rhs_arr)
+            else:
+                rules[nonterm].append(rhs_arr)
     return rules
+
+
+def replace_escape_char_nt(nonterms):
+    nonterms_map = dict()
+    special_chars = " ()\'_\\,."
+    replace_chars = {
+        '-': 'minus',
+        '+': 'plus',
+        '?': 'optional',
+        '|': 'or',
+        '*': 'star'
+    }
+    for nt in nonterms:
+        if nt.isalpha():
+            nonterms_map[nt] = nt
+        else:
+            new_str = nt
+            for c in special_chars:
+                new_str = new_str.replace(c, "")
+            for c in replace_chars:
+                new_str = new_str.replace(c, replace_chars[c])
+            nonterms_map[nt] = new_str
+    return nonterms_map
+
+
+def unquote_t(terms):
+    terms_map = dict()
+    for t in terms:
+        if t[0] == "\'" and t[-1] == "\'":
+            terms_map[t] = t[1:-1]
+        else:
+            terms_map[t] = t
+    return terms_map
 
 
 if __name__ == "__main__":
@@ -73,12 +108,43 @@ if __name__ == "__main__":
     f = open(filename, 'r')
 
     nt = parse_nonterms(f)
+    nt_map = replace_escape_char_nt(nt)
     t = parse_terms(f)
+    t_map = unquote_t(t)
     rules = parse_rules(f, t, nt)
-    name = re.split('.')[0]
-    out = open(name+".y", 'w')
+    name = re.split('.', filename)[0]
+    out = open(name + ".y", 'w')
+    lines = []
     for rule in rules:
         line = ""
-        line += rule
+        line += nt_map[rule] + ": "
+        offset_spaces = len(line)
+        # deal with epsilon
         for sub in rules[rule]:
-            
+            cur_t_str = ""
+            for symbol in sub:
+                if symbol in nt:
+                    if len(cur_t_str) > 0:
+                        line += "\'"+cur_t_str + "\' "
+                        cur_t_str = ""
+                    line += nt_map[symbol] + " "
+                elif symbol in t:
+                    cur_t_str += t_map[symbol]
+            if len(cur_t_str) > 0:
+                line += "\'"+cur_t_str + "\' "
+            line += "|"
+            lines.append(line)
+            line = " " * offset_spaces
+        # chop of last |
+        lines[-1] = lines[-1][:-1]
+        lines.append(" " * (offset_spaces - 2) + ";")
+        lines.append("")
+
+    print("%%")
+    print()
+
+    for line in lines:
+        print(line)
+
+    print()
+    print("%%")
